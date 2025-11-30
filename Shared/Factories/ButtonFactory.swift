ba299c0c2f73b56e3173bd7487786074f74a966f
georgiiIaroshenko <georg2991@mail.ru>
2025-11-30 22:04:33 +0300

    refactor(ButtonFactory): use UIButton.Configuration.filled()

--- Shared/Factories/ButtonFactory.swift
+++ Shared/Factories/ButtonFactory.swift
@@ -6,35 +6,48 @@ struct ButtonConfig {
     var cornerRadius: CGFloat?
     var backgroundColor: UIColor?
     var tintColor: UIColor?
-
+    var contentInsets: NSDirectionalEdgeInsets?
+    
     init(contentMode: UIView.ContentMode = .scaleAspectFill,
          clipsToBounds: Bool = true,
          cornerRadius: CGFloat? = nil,
          backgroundColor: UIColor? = nil,
-         tintColor: UIColor? = nil) {
+         tintColor: UIColor? = nil,
+         contentInsets: NSDirectionalEdgeInsets? = nil) {
         self.contentMode = contentMode
         self.clipsToBounds = clipsToBounds
         self.cornerRadius = cornerRadius
         self.backgroundColor = backgroundColor
         self.tintColor = tintColor
+        self.contentInsets = contentInsets
     }
 }
 
 final class ButtonFactory {
     @discardableResult
     func make(_ config: ButtonConfig = .init()) -> UIButton {
-        let button = UIButton()
-        button.contentMode = config.contentMode
-        button.clipsToBounds = config.clipsToBounds
-        if let radius = config.cornerRadius {
-            button.layer.cornerRadius = radius
-        }
+        var configuration = UIButton.Configuration.filled()
+        
         if let bg = config.backgroundColor {
-            button.backgroundColor = bg
+            configuration.baseBackgroundColor = bg
         }
         if let tint = config.tintColor {
-            button.tintColor = tint
+            configuration.baseForegroundColor = tint
         }
+        
+        if let insets = config.contentInsets {
+            configuration.contentInsets = insets
+        }
+        
+        if let radius = config.cornerRadius {
+            configuration.cornerStyle = .fixed
+            configuration.background.cornerRadius = radius
+        }
+        
+        let button = UIButton(configuration: configuration)
+        button.contentMode = config.contentMode
+        button.clipsToBounds = config.clipsToBounds
+        
         return button
     }
 }
